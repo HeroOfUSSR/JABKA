d9a9049d7c6e8c543a2c9dadada4f74806da399c
HeroOfUSSR <alexey_ivanov04@mail.ru>
2023-01-11 10:57:02 +0300

    biba

---
+++ JABKA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JABKA
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person();  // создание объекта класса Person
+
+            // Получаем значение полей в переменные
+            string personName = tom.name;
+            int personAge = tom.age;
+            Console.WriteLine($"Имя: {personName}  Возраст {personAge}");   // Имя: Undefined  Возраст: 0
+
+            // устанавливаем новые значения полей
+            tom.name = Console.ReadLine();
+            var age = Console.ReadLine();
+            bool result = int.TryParse(age, out var number);
+            tom.age = number;
+
+            new 
+            // обращаемся к методу Print
+            tom.Print();    // Имя: Tom  Возраст: 37
+        }
+    }
+}
