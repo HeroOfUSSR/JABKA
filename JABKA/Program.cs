2100dd26db5d0f84eb32add084249866e36e0555
HeroOfUSSR <alexey_ivanov04@mail.ru>
2023-01-11 12:02:58 +0300

    AE

--- JABKA/Program.cs
+++ JABKA/Program.cs
@@ -9,20 +9,64 @@ namespace JABKA
         {
             Person tom = new Person();  // создание объекта класса Person
 
-            // Получаем значение полей в переменные
-            string personName = tom.name;
-            int personAge = tom.age;
-            Console.WriteLine($"Имя: {personName}  Возраст {personAge}");   // Имя: Undefined  Возраст: 0
+            int count = GetNumber("Print count");
 
-            // устанавливаем новые значения полей
-            tom.name = Console.ReadLine();
-            var age = Console.ReadLine();
-            bool result = int.TryParse(age, out var number);
-            tom.age = number;
+            var People = new Person[count];
 
-            new 
-            // обращаемся к методу Print
-            tom.Print();    // Имя: Tom  Возраст: 37
+            for (int i = 0; i < count; i++)
+            {
+                var person = new Person();
+                People[i] = person;
+                Console.WriteLine("Print name");
+                person.name = Console.ReadLine();
+                person.age = GetNumber("Print age");
+
+            }
+
+            Sort(People);
+
+            foreach (var person in People)
+            {
+                person.Print();
+            }
+
+            Console.ReadKey();
+
+        }
+
+        private static void Sort(Person[] cycle )
+        {
+            for (int i = 0; i < cycle.Length - 1; i++)
+            {
+                for (int j = i + 1; j < cycle.Length; j++)
+                {
+                    if (cycle[i].age > cycle[j].age)
+                    {
+                        var temp = cycle[i];
+                        cycle[i] = cycle[j];
+                        cycle[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+        private static int GetNumber(string printMessage)
+        {
+            int number;
+            while (true)
+            {
+                Console.WriteLine(printMessage);
+                var countStr = Console.ReadLine();
+
+                if (int.TryParse(countStr, out number))
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{countStr} is not a number");
+            }
+            return number;
         }
     }
 }
